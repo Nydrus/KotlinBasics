1553726ca38b31b5b200237f1591e0b229348391
Miguel Raposo <mickaelslb@gmail.com>
2024-12-26 21:58:22 +0000

    Add divide and add function

--- app/src/main/java/eu/develop/functions/Basics.kt
+++ app/src/main/java/eu/develop/functions/Basics.kt
@@ -1,13 +1,26 @@
 package eu.develop.functions
 
+import androidx.core.graphics.convertTo
+
 fun main(){
+
+    var daisy = Dog()
+}
+
+fun add(num1:Int, num2:Int):Int{
+   val result = num1 + num2
+    return result
+}
+fun divide(num1:Double, num2:Double): Double{
+    val result = num1/num2
+    return result
+}
+fun askCoffeeDetails(){
     println("Who is this coffee for?")
     val name = readln()
     println("How many pieces of sugar does $name want?")
     val sugarCount = readln()
     val sugarCountInt = sugarCount.toInt()
-
-
     makeCoffee(sugarCountInt,name)
 }
 fun makeCoffee(sugarCount : Int, name: String){
